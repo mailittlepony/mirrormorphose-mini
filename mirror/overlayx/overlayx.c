ee6794173217f3bd709a419fa05b376c3a851bbb
stantonik <stanleyy.arnaud@gmail.com>
2025-07-04 15:48:22 +0900

    add vignette

--- mirror/overlayx/overlayx.c
+++ mirror/overlayx/overlayx.c
@@ -30,16 +30,20 @@
 // -----------------------------------------------------------------------------
 #define IMAGE_FORMAT VC_IMAGE_RGBA16
 
+#define FADE_LAYER 2
+#define VIGNETTE_LAYER 3
+
 // -----------------------------------------------------------------------------
 // Static Variables
 // -----------------------------------------------------------------------------
 static DISPMANX_DISPLAY_HANDLE_T display;
-static DISPMANX_UPDATE_HANDLE_T update;
-static DISPMANX_RESOURCE_HANDLE_T resource;
-static DISPMANX_ELEMENT_HANDLE_T element;
+
+static DISPMANX_RESOURCE_HANDLE_T vignette_res;
+static DISPMANX_ELEMENT_HANDLE_T vignette_elmt;
+static DISPMANX_RESOURCE_HANDLE_T fade_res;
+static DISPMANX_ELEMENT_HANDLE_T fade_elmt;
 
 static uint32_t screen_width, screen_height;
-static uint16_t *image = NULL;
 static int display_num = 0;
 
 // -----------------------------------------------------------------------------
@@ -64,32 +68,76 @@ int overlay_init(const char *img_path)
         return 1;
     }
 
+    // Resources allocation
     uint32_t vc_image_ptr;
-    resource = vc_dispmanx_resource_create(IMAGE_FORMAT, screen_width,
+    vignette_res = vc_dispmanx_resource_create(IMAGE_FORMAT, screen_width,
+                                               screen_height, &vc_image_ptr);
+
+    fade_res = vc_dispmanx_resource_create(IMAGE_FORMAT, screen_width,
                                            screen_height, &vc_image_ptr);
-    if (!resource)
+    if (!vignette_res || !fade_res)
     {
-        fprintf(stderr, "Failed to create resource\n");
+        fprintf(stderr, "Failed to create resources\n");
         return 1;
     }
 
-    int pitch = screen_width * sizeof(uint16_t);
-    image = malloc(pitch * screen_height);
-    if (image == NULL)
+    VC_RECT_T rect;
+    vc_dispmanx_rect_set(&rect, 0, 0, screen_width, screen_height);
+
+    // Create fade overlay
+    int pitch = (screen_width * 2 + 31) & ~31;
+    uint16_t *fade_img = malloc(pitch * screen_height);
+    if (fade_img == NULL)
     {
-        fprintf(stderr, "Memory allocation failed\n");
+        fprintf(stderr, "Memory allocation failed for fade image\n");
         return 1;
     }
 
     for (int i = 0; i < screen_width * screen_height; i++)
     {
-        image[i] = 0x000F;
+        fade_img[i] = 0x000F;
     }
 
-    VC_RECT_T rect;
-    vc_dispmanx_rect_set(&rect, 0, 0, screen_width, screen_height);
-    vc_dispmanx_resource_write_data(resource, IMAGE_FORMAT, pitch, image,
+    vc_dispmanx_resource_write_data(fade_res, IMAGE_FORMAT, pitch, fade_img,
                                     &rect);
+    free(fade_img);
+
+    // Create vignette overlay
+    int width, height, chans;
+    uint16_t *vignette_img = malloc(pitch * screen_height);
+    uint8_t *data = stbi_load(img_path, &width, &height, &chans, 0);
+    if (vignette_img == NULL | data == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed for vignette image\n");
+        return 1;
+    }
+
+    if (width != screen_width || height != screen_height)
+    {
+        fprintf(stderr, "Vignette image size is not the same as the screen size");
+        return 1;
+    }
+
+    for (int y = 0; y < screen_height; y++)
+    {
+        for (int x = 0; x < screen_width; x++)
+        {
+            int src_i = (y * width + x) * 4;
+            int dst_i = y * (pitch / 2) + x;
+
+            uint8_t r = data[src_i + 0] >> 4;
+            uint8_t g = data[src_i + 1] >> 4;
+            uint8_t b = data[src_i + 2] >> 4;
+            uint8_t a = data[src_i + 3] >> 4;
+
+            // Pack into R4G4B4A4 format: RRRRGGGGBBBBAAAA
+            vignette_img[dst_i] = (r << 12) | (g << 8) | (b << 4) | a;
+        }
+    }
+    free(data);
+    vc_dispmanx_resource_write_data(vignette_res, IMAGE_FORMAT, pitch,
+                                    vignette_img, &rect);
+    free(vignette_img);
 
     display = vc_dispmanx_display_open(display_num);
     if (!display)
@@ -98,29 +146,27 @@ int overlay_init(const char *img_path)
         return 1;
     }
 
-    update = vc_dispmanx_update_start(display_num);
-    if (!update)
-    {
-        fprintf(stderr, "Failed to start update\n");
-        return 1;
-    }
-
     VC_RECT_T src_rect;
     vc_dispmanx_rect_set(&src_rect, 0, 0, screen_width << 16,
                          screen_height << 16);
-
     VC_RECT_T dst_rect;
     vc_dispmanx_rect_set(&dst_rect, 0, 0, screen_width, screen_height);
 
-    VC_DISPMANX_ALPHA_T alpha = {
+    VC_DISPMANX_ALPHA_T fade_alpha = {
         .flags = DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS,
         .opacity = 255,
         .mask = 0,
     };
 
-    element = vc_dispmanx_element_add(update, display, 0, &dst_rect, resource,
-                                      &src_rect, DISPMANX_PROTECTION_NONE,
-                                      &alpha, NULL, VC_IMAGE_ROT0);
+    DISPMANX_UPDATE_HANDLE_T update = vc_dispmanx_update_start(display_num);
+
+    vignette_elmt = vc_dispmanx_element_add(
+        update, display, VIGNETTE_LAYER, &dst_rect, vignette_res, &src_rect,
+        DISPMANX_PROTECTION_NONE, NULL, NULL, VC_IMAGE_ROT0);
+
+    fade_elmt = vc_dispmanx_element_add(update, display, FADE_LAYER, &dst_rect, fade_res,
+                                        &src_rect, DISPMANX_PROTECTION_NONE,
+                                        &fade_alpha, NULL, VC_IMAGE_ROT0);
 
     vc_dispmanx_update_submit_sync(update);
 }
@@ -131,9 +177,9 @@ int overlay_start_fade_in(int duration_ms, int step)
 
     for (int a = 255; a >= 0; a -= step)
     {
-        update = vc_dispmanx_update_start(0);
-        vc_dispmanx_element_change_attributes(update, element, 2, 0, (uint8_t)a,
-                                              NULL, NULL, 0, 0);
+        DISPMANX_UPDATE_HANDLE_T update = vc_dispmanx_update_start(display_num);
+        vc_dispmanx_element_change_attributes(update, fade_elmt, 2, FADE_LAYER,
+                                              (uint8_t)a, NULL, NULL, 0, 0);
         vc_dispmanx_update_submit_sync(update);
         usleep(delay_us);
     }
@@ -145,9 +191,9 @@ int overlay_start_fade_out(int duration_ms, int step)
 
     for (int a = 0; a < 255; a += step)
     {
-        update = vc_dispmanx_update_start(0);
-        vc_dispmanx_element_change_attributes(update, element, 2, 0, (uint8_t)a,
-                                              NULL, NULL, 0, 0);
+        DISPMANX_UPDATE_HANDLE_T update = vc_dispmanx_update_start(display_num);
+        vc_dispmanx_element_change_attributes(update, fade_elmt, 2, FADE_LAYER,
+                                              (uint8_t)a, NULL, NULL, 0, 0);
         vc_dispmanx_update_submit_sync(update);
         usleep(delay_us);
     }
@@ -155,11 +201,12 @@ int overlay_start_fade_out(int duration_ms, int step)
 
 int overlay_free(void)
 {
-    update = vc_dispmanx_update_start(0);
-    vc_dispmanx_element_remove(update, element);
+    DISPMANX_UPDATE_HANDLE_T update = vc_dispmanx_update_start(0);
+    vc_dispmanx_element_remove(update, vignette_elmt);
+    vc_dispmanx_element_remove(update, fade_elmt);
     vc_dispmanx_update_submit_sync(update);
 
-    vc_dispmanx_resource_delete(resource);
+    vc_dispmanx_resource_delete(vignette_res);
+    vc_dispmanx_resource_delete(fade_res);
     vc_dispmanx_display_close(display);
-    free(image);
 }
