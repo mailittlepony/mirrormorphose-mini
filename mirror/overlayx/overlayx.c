507a0590491866072f3099c9fa581bee8ffd5529
stantonik <stanleyy.arnaud@gmail.com>
2025-07-04 19:17:58 +0900

    vignette is now optional

--- mirror/overlayx/overlayx.c
+++ mirror/overlayx/overlayx.c
@@ -61,6 +61,14 @@ int overlay_init(const char *img_path)
 {
     bcm_host_init();
 
+    // Create display and get size
+    display = vc_dispmanx_display_open(display_num);
+    if (!display)
+    {
+        fprintf(stderr, "Failed to open display\n");
+        return 1;
+    }
+
     if (graphics_get_display_size(display_num, &screen_width, &screen_height) <
         0)
     {
@@ -68,21 +76,21 @@ int overlay_init(const char *img_path)
         return 1;
     }
 
+    VC_RECT_T src_rect;
+    vc_dispmanx_rect_set(&src_rect, 0, 0, screen_width << 16,
+                         screen_height << 16);
+    VC_RECT_T dst_rect;
+    vc_dispmanx_rect_set(&dst_rect, 0, 0, screen_width, screen_height);
+
     // Resources allocation
     uint32_t vc_image_ptr;
-    vignette_res = vc_dispmanx_resource_create(IMAGE_FORMAT, screen_width,
-                                               screen_height, &vc_image_ptr);
-
     fade_res = vc_dispmanx_resource_create(IMAGE_FORMAT, screen_width,
                                            screen_height, &vc_image_ptr);
-    if (!vignette_res || !fade_res)
+    if (fade_res)
     {
-        fprintf(stderr, "Failed to create resources\n");
+        fprintf(stderr, "Failed to create resource\n");
         return 1;
     }
-
-    VC_RECT_T rect;
-    vc_dispmanx_rect_set(&rect, 0, 0, screen_width, screen_height);
 
     // Create fade overlay
     int pitch = (screen_width * 2 + 31) & ~31;
@@ -99,58 +107,58 @@ int overlay_init(const char *img_path)
     }
 
     vc_dispmanx_resource_write_data(fade_res, IMAGE_FORMAT, pitch, fade_img,
-                                    &rect);
+                                    &dst_rect);
     free(fade_img);
 
     // Create vignette overlay
-    int width, height, chans;
-    uint16_t *vignette_img = malloc(pitch * screen_height);
-    uint8_t *data = stbi_load(img_path, &width, &height, &chans, 0);
-    if (vignette_img == NULL | data == NULL)
+    if (strlen(img_path) > 0)
     {
-        fprintf(stderr, "Memory allocation failed for vignette image\n");
-        return 1;
-    }
+        vignette_res = vc_dispmanx_resource_create(
+            IMAGE_FORMAT, screen_width, screen_height, &vc_image_ptr);
 
-    if (width != screen_width || height != screen_height)
-    {
-        fprintf(stderr, "Vignette image size is not the same as the screen size");
-        return 1;
-    }
-
-    for (int y = 0; y < screen_height; y++)
-    {
-        for (int x = 0; x < screen_width; x++)
+        if (vignette_res)
         {
-            int src_i = (y * width + x) * 4;
-            int dst_i = y * (pitch / 2) + x;
-
-            uint8_t r = data[src_i + 0] >> 4;
-            uint8_t g = data[src_i + 1] >> 4;
-            uint8_t b = data[src_i + 2] >> 4;
-            uint8_t a = data[src_i + 3] >> 4;
-
-            // Pack into R4G4B4A4 format: RRRRGGGGBBBBAAAA
-            vignette_img[dst_i] = (r << 12) | (g << 8) | (b << 4) | a;
+            fprintf(stderr, "Failed to create resource\n");
+            return 1;
         }
-    }
-    free(data);
-    vc_dispmanx_resource_write_data(vignette_res, IMAGE_FORMAT, pitch,
-                                    vignette_img, &rect);
-    free(vignette_img);
 
-    display = vc_dispmanx_display_open(display_num);
-    if (!display)
-    {
-        fprintf(stderr, "Failed to open display\n");
-        return 1;
-    }
+        int width, height, chans;
+        uint16_t *vignette_img = malloc(pitch * screen_height);
+        uint8_t *data = stbi_load(img_path, &width, &height, &chans, 0);
+        if (vignette_img == NULL | data == NULL)
+        {
+            fprintf(stderr, "Memory allocation failed for vignette image\n");
+            return 1;
+        }
 
-    VC_RECT_T src_rect;
-    vc_dispmanx_rect_set(&src_rect, 0, 0, screen_width << 16,
-                         screen_height << 16);
-    VC_RECT_T dst_rect;
-    vc_dispmanx_rect_set(&dst_rect, 0, 0, screen_width, screen_height);
+        if (width != screen_width || height != screen_height)
+        {
+            fprintf(stderr,
+                    "Vignette image size is not the same as the screen size");
+            return 1;
+        }
+
+        for (int y = 0; y < screen_height; y++)
+        {
+            for (int x = 0; x < screen_width; x++)
+            {
+                int src_i = (y * width + x) * 4;
+                int dst_i = y * (pitch / 2) + x;
+
+                uint8_t r = data[src_i + 0] >> 4;
+                uint8_t g = data[src_i + 1] >> 4;
+                uint8_t b = data[src_i + 2] >> 4;
+                uint8_t a = data[src_i + 3] >> 4;
+
+                // Pack into R4G4B4A4 format: RRRRGGGGBBBBAAAA
+                vignette_img[dst_i] = (r << 12) | (g << 8) | (b << 4) | a;
+            }
+        }
+        free(data);
+        vc_dispmanx_resource_write_data(vignette_res, IMAGE_FORMAT, pitch,
+                                        vignette_img, &dst_rect);
+        free(vignette_img);
+    }
 
     VC_DISPMANX_ALPHA_T fade_alpha = {
         .flags = DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS,
@@ -160,13 +168,16 @@ int overlay_init(const char *img_path)
 
     DISPMANX_UPDATE_HANDLE_T update = vc_dispmanx_update_start(display_num);
 
-    vignette_elmt = vc_dispmanx_element_add(
-        update, display, VIGNETTE_LAYER, &dst_rect, vignette_res, &src_rect,
-        DISPMANX_PROTECTION_NONE, NULL, NULL, VC_IMAGE_ROT0);
+    if (strlen(img_path) > 0)
+    {
+        vignette_elmt = vc_dispmanx_element_add(
+            update, display, VIGNETTE_LAYER, &dst_rect, vignette_res, &src_rect,
+            DISPMANX_PROTECTION_NONE, NULL, NULL, VC_IMAGE_ROT0);
+    }
 
-    fade_elmt = vc_dispmanx_element_add(update, display, FADE_LAYER, &dst_rect, fade_res,
-                                        &src_rect, DISPMANX_PROTECTION_NONE,
-                                        &fade_alpha, NULL, VC_IMAGE_ROT0);
+    fade_elmt = vc_dispmanx_element_add(
+        update, display, FADE_LAYER, &dst_rect, fade_res, &src_rect,
+        DISPMANX_PROTECTION_NONE, &fade_alpha, NULL, VC_IMAGE_ROT0);
 
     vc_dispmanx_update_submit_sync(update);
 
